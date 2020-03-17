3242a347a0e2e656aeb8804fb9e524944a554619
Zongqi Wang <wangzongqi@msn.com>
2020-03-17 14:15:06 -0700

    reversed changes

---
+++ assets/js/vue/portfolio.js
@@ -0,0 +1,12 @@
+var vm = new Vue({
+
+    el: '#port',
+    data:{
+        works:[
+            { text: 'Learn JavaScript',
+            link: 'google.com' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+          ]
+    }
+})
